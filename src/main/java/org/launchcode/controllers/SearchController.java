e56e0563afe950e7b634e8241bdb6b934952d3f7
Ruixian Shan <shanruixian@gmail.com>
2017-03-23 01:25:59 -0500

    create handler to process search request and display results; in search.html loop over jobs map to display search results

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static org.launchcode.controllers.ListController.columnChoices;
+
 /**
  * Created by LaunchCode
  */
@@ -18,10 +20,25 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
-        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("columns", columnChoices);
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String results(Model model,
+                          @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        String[] displayColumns={"position type","name","employer","location","core competency"};
+
+        model.addAttribute("displayColumns", displayColumns);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("results",jobs.size()+ " Result(s)");
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
+
 
 }
